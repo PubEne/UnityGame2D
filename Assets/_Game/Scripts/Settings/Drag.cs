25fa19d498bf80f84001d463a0babc0323d03d4e
PubEne <pawelec.piotrek@gmail.com>
2020-03-14 19:56:03 +0100

    Settings POC and drag on event for controls

---
+++ Assets/_Game/Scripts/Settings/Drag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag :  EventTrigger
+{
+    private bool dragging;
+    public void Update()
+    {
+        if (dragging && Globals.allowDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        dragging = true;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        dragging = false;
+    }
+}
---
+++ Assets/_Game/Scripts/Settings/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public static bool allowDragging;
+}
+
+public class Settings : MonoBehaviour
+{
+    public void Start()
+    {
+
+    }
+
+    public void DraggingOn(bool value)
+    {
+        if (value == true)
+        {
+            Globals.allowDragging = true;
+        }
+        else
+        {
+            Globals.allowDragging = false;
+        }
+    }
+}
