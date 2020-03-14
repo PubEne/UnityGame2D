8931f0bbe8a649ae6f7f2538df75c2e76ef72d59
PubEne <pawelec.piotrek@gmail.com>
2020-03-14 19:45:44 +0100

    Menu POC navigation added.

---
+++ Assets/_Game/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject OptionsMenu;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Options()
+    {
+        OptionsMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/_Game/Scripts/GUI/OptionsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainMenu;
+
+    public void Option_1()
+    {
+        Debug.Log("Option_1");
+    }
+
+    public void Option_2()
+    {
+        Debug.Log("Option_1");
+    }
+
+    public void Option_3()
+    {
+        Debug.Log("Option_1");
+    }
+
+    public void Back()
+    {
+        MainMenu.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
